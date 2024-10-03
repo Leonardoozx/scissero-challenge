2ea0673b7eb5b275491c25a4a5ca752e3dca21bf
Leonardo Guimarães Urci <leonardoguimaraesurci@gmail.com>
2024-10-03 20:54:00 -0300

    feat: uses utill instead of file-local function

--- src/generateDocx.mjs
+++ src/generateDocx.mjs
@@ -1,19 +1,5 @@
-// import { capitalizeWords } from './utils/captalizeWords.mjs';
+import { capitalizeWords } from './utils/captalizeWords.mjs';
 import DocxHandler from './utils/docxHandler.mjs';
-
-/**
- * Captalizes all the first letters in the given string, example:
- * 
- * input: 'hi I like gym'
- * output: 'Hi I Like Gym' 
- * 
- * @param {string} str
- */
-const capitalizeWords = (str) => {
-  return str.split(' ').map(word => {
-      return word.charAt(0).toUpperCase() + word.slice(1);
-  }).join(' ');
-}
 
 export default async (inputData) => {
   const document = new DocxHandler(inputData);
